71a62bf95fd5e13379e4d8e447c3f7a3e7955b34
wxd123456789 <1042873383@qq.com>
2022-05-15 12:28:21 +0800

    2022/05/15 周日

---
+++ git.bat
@@ -0,0 +1,5 @@
+"E:\Program Files\Git\bin\git" status
+"E:\Program Files\Git\bin\git" add .
+"E:\Program Files\Git\bin\git" commit -m "%date%"
+"E:\Program Files\Git\bin\git" push origin master
+pause
